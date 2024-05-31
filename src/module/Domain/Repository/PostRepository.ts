43f8a879cc493d66477fc22742b2f3a63aa4afb5
unknown <doyinadeyemi2000@gmail.com>
2024-05-31 14:21:48 +0100

    added user name to post request

--- src/module/Domain/Repository/PostRepository.ts
+++ src/module/Domain/Repository/PostRepository.ts
@@ -23,18 +23,69 @@ export class PostRepository implements IPostRepository {
     async getPost(postId: string): Promise<Post> {
         const post = await this.database
             .collection<Post>('posts')
-            .findOne({ postId });
-        return post as Post;
+            .aggregate([
+                { $match: { postId, status: PostStatus.ACTIVE } },
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: 'userId',
+                        foreignField: 'userId',
+                        as: 'userDetails',
+                    },
+                },
+                {
+                    $project: {
+                        userId: 1,
+                        postId: 1,
+                        fromGuest: 1,
+                        post: 1,
+                        status: 1,
+                        createdOn: 1,
+                        lastModifiedOn: 1,
+                        createdBy: 1,
+                        modifiedBy: 1,
+                        'userDetails.userName': 1,
+                    },
+                },
+            ])
+            .toArray();
+
+        return post[0] as Post;
     }
 
     async getPosts(userId?: string): Promise<Post[]> {
-        const query: any = {};
+        const query: any = { status: PostStatus.ACTIVE };
         if (userId) {
             query.userId = userId;
         }
+
         const posts = await this.database
             .collection<Post>('posts')
-            .find(query)
+            .aggregate([
+                { $match: { ...query } },
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: 'userId',
+                        foreignField: 'userId',
+                        as: 'userDetails',
+                    },
+                },
+                {
+                    $project: {
+                        userId: 1,
+                        postId: 1,
+                        fromGuest: 1,
+                        post: 1,
+                        status: 1,
+                        createdOn: 1,
+                        lastModifiedOn: 1,
+                        createdBy: 1,
+                        modifiedBy: 1,
+                        'userDetails.userName': 1,
+                    },
+                },
+            ])
             .toArray();
         return posts as Post[];
     }
